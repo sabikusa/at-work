1420040caf994322ac4a86798d8b62e33672e5fa
sabikusa <nakayama.shinya@metlife.co.jp>
2020-12-21 17:29:05 +0900

    rev1

--- ISE_lookup.py
+++ ISE_lookup.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 import requests
 import urllib3
+import xml.dom.minidom
 
-'''This script is simple API caller for MAC registration to ISE'''
+'''This script is simple API caller for MAC lookup to ISE'''
 
 #disabling SSL error
 urllib3.disable_warnings()
 
-url = 'https://szlnm189dha:9060/ers/config/endpoint/bulk'
-body1 = '''\
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<ns4:endpointBulkRequest operationType="create" resourceMediaType="vnd.com.cisco.ise.identity.endpoint.1.0+xml" xmlns:ns4="identity.ers.ise.cisco.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-        <ns4:resourcesList>
-'''
+url = 'https://szlnm189dha:9060/ers/config/endpoint/name/'
+hdr = {
+        'Accept': 'application/xml',
+        'Authorization': 'Basic ZXJzYWRtaW46RW1ndXl0aDVhZw==',
+        'Content-Type': 'application/xml'
+        }
+
+#MAC you wish to lookup
+mac = input('Enter a MAC -> ')
+
+res = requests.get(url + mac, headers = hdr, verify = False)
+print(res.status_code)
+output = xml.dom.minidom.parseString(res.text)
+print(output.toprettyxml())
+
+
 
