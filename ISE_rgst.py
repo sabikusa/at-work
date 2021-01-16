241fb78882294abb2edda5559debe5b0100a4b38
sabikusa <nakayama.shinya@metlife.co.jp>
2021-01-16 18:44:27 +0900

    ss

--- ISE_rgst.py
+++ ISE_rgst.py
@@ -12,33 +12,34 @@ import sys
 #disabling SSL error
 urllib3.disable_warnings()
 
-#URI to call API to
-url = "https://szlnm189ata:9060/ers/config/endpoint/bulk"
-
 payload = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><ns4:endpointBulkRequest operationType="create"\
     resourceMediaType="vnd.com.cisco.ise.identity.endpoint.1.0+xml" xmlns:ns4="identity.ers.ise.cisco.com" xmlns:xsi=\
         "http://www.w3.org/2001/XMLSchema-instance"><ns4:resourcesList>'
-
 payload2 = "</ns4:resourcesList></ns4:endpointBulkRequest>"
-
-headers = {
-  'Accept': 'application/xml',
-  'Authorization': 'Basic ZXJzYWRtaW46RW1ndXl0aDVhZw==',
-  'Content-Type': 'application/xml'
-}
 
 def main():
     args = sys.argv
+
     for ise in enumerate(args):
         ep = f"<ns4:endpoint><groupId>9d934d30-644c-11ea-a8ef-12e4691882e8</groupId><mac>{ise}</mac>\
             <staticGroupAssignment>true</staticGroupAssignment>\
             <staticProfileAssignment>false</staticProfileAssignment></ns4:endpoint>"
         payload += ep
         print('-' * 30)
-        print('MAC{:02}'.format(ise))
+        print('MAC{:02}'.format(str(ise))
+    
 
-    payload += payload2
+    #URI to call API to
+    url = "https://szlnm189ata:9060/ers/config/endpoint/bulk"
 
+    headers = {
+    'Accept': 'application/xml',
+    'Authorization': 'Basic ZXJzYWRtaW46RW1ndXl0aDVhZw==',
+    'Content-Type': 'application/xml'
+    }
+
+    payload += playload2
+    
     # calling an API to ISE with the parameter
     response = requests.put(url, headers = headers, data = payload, verify = False)
 
