d9fe0cd019187d18f71e47992802b6946da80b76
Shinya <regulate0109@gmail.com>
2021-01-16 17:54:05 +0900

    rgst

---
+++ ISE_rgst.py
@@ -0,0 +1,61 @@
+#!usr/bin/env python3
+
+import requests
+import urllib3
+import xml.dom.minidom
+import time
+from datetime import datetime
+import xmltodict
+from pprint import pprint
+import sys
+
+#disabling SSL error
+urllib3.disable_warnings()
+
+#URI to call API to
+url = "https://szlnm189ata:9060/ers/config/endpoint/bulk"
+
+payload = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><ns4:endpointBulkRequest operationType="create"\
+    resourceMediaType="vnd.com.cisco.ise.identity.endpoint.1.0+xml" xmlns:ns4="identity.ers.ise.cisco.com" xmlns:xsi=\
+        "http://www.w3.org/2001/XMLSchema-instance"><ns4:resourcesList>'
+
+payload2 = "</ns4:resourcesList></ns4:endpointBulkRequest>"
+
+headers = {
+  'Accept': 'application/xml',
+  'Authorization': 'Basic ZXJzYWRtaW46RW1ndXl0aDVhZw==',
+  'Content-Type': 'application/xml'
+}
+
+def main():
+    args = sys.argv
+    for i, arg in enumerate(args):
+        ep = f"<ns4:endpoint><groupId>9d934d30-644c-11ea-a8ef-12e4691882e8</groupId><mac>{ise}</mac>\
+            <staticGroupAssignment>true</staticGroupAssignment>\
+            <staticProfileAssignment>false</staticProfileAssignment></ns4:endpoint>"
+        payload += ep
+        print('-' * 30)
+        print('MAC{:02}'.format(i))
+
+    payload += payload2
+
+    # calling an API to ISE with the parameter
+    response = requests.put(url, headers = headers, data = payload, verify = False)
+
+    # getting a bulk status based on the response header from ISE
+    inquiry = requests.get(response.headers['Location'], headers = headers, verify = False)
+
+    print("response_status = ", response.status_code)
+
+    doc = xmltodict.parse(inquiry.text)
+    pprint(doc['ns2:bulkStatus']['ns2:resourceStatus']['@id'])
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
