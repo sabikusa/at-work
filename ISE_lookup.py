54d8718a4da9412fb8c6e0c150664b633a3d1fff
Shinya <regulate0109@gmail.com>
2021-01-16 09:56:06 +0900

    argtest

--- ISE_lookup.py
+++ ISE_lookup.py
@@ -2,11 +2,16 @@
 import requests
 import urllib3
 import xml.dom.minidom
+import argparse
 
 '''This script is simple API caller for MAC lookup to ISE'''
 
 #disabling SSL error
 urllib3.disable_warnings()
+
+parser = argparse.ArgumentParser(description='MAC registration checker')
+parser.add_argument('MAC', type=str, help='MAC you wish to check')
+arg = parser.parse_args()
 
 url = 'https://szlnm189dha:9060/ers/config/endpoint/name/'
 hdr = {
@@ -16,12 +21,26 @@ hdr = {
         }
 
 #MAC you wish to lookup
-mac = input('Enter a MAC -> ')
+#mac = input('Enter a MAC -> ')
 
-res = requests.get(url + mac, headers = hdr, verify = False)
-print('resopnse code :', res.status_code)
-output = xml.dom.minidom.parseString(res.text)
-print(output.toprettyxml())
+def check(MAC):
+        res = requests.get(url + MAC, headers = hdr, verify = False)
+        print('resopnse code :', res.status_code)
+        output = xml.dom.minidom.parseString(res.text)
+        print(output.toprettyxml())
+
+if __name__ == '__main__':
+        check(arg.MAC)
+
+
+
+
+
+
+# res = requests.get(url + mac, headers = hdr, verify = False)
+# print('resopnse code :', res.status_code)
+# output = xml.dom.minidom.parseString(res.text)
+# print(output.toprettyxml())
 
 
 
