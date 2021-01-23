da3558057a3fcb3443f68228556653bfe8e976cf
sabikusa <nakayama.shinya@metlife.co.jp>
2021-01-23 17:32:46 +0900

    ct

--- ISE_rgst.py
+++ ISE_rgst.py
@@ -26,7 +26,7 @@ def main():
     resourceMediaType="vnd.com.cisco.ise.identity.endpoint.1.0+xml" xmlns:ns4="identity.ers.ise.cisco.com" xmlns:xsi=\
         "http://www.w3.org/2001/XMLSchema-instance"><ns4:resourcesList>'
     payload2 = "</ns4:resourcesList></ns4:endpointBulkRequest>"
-    for ise, arg in enumerate(args):
+    for ise, arg in enumerate(args[1:]):
         ep = f"<ns4:endpoint><groupId>9d934d30-644c-11ea-a8ef-12e4691882e8</groupId><mac>{arg}</mac>\
             <staticGroupAssignment>true</staticGroupAssignment>\
             <staticProfileAssignment>false</staticProfileAssignment></ns4:endpoint>"
